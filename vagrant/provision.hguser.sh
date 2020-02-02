3b5fff84f4617f7ae5a1b4dc5f602d4278e0f8ed
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-02-02 20:11:58 +0900

    ユーザー hg@vagrant を作成

---
+++ vagrant/provision.hguser.sh
@@ -0,0 +1,34 @@
+#! /bin/bash  -xue
+
+echo  Provisioning  ${USER} for ${HOSTNAME}
+test  -f  ${HOME}/.provision.user  &&  exit 0
+
+###
+##  仮想マシン内に作成するユーザーのパスワード
+###
+
+userPass='\$6\$F1IrDR2U\$'
+userPass+='gdsuepzT7noxUWaA5cpRjfAdhE5cS6qh0WhNBYm83ey'
+userPass+='PiZ0XbfrzuG6dGH3aezIQWn.vRf.aJsz1qD3RMi7yj0'
+
+newUser=hg
+newUserGroup=hg
+
+sudo  groupadd  ${newUserGroup}
+userIndex=0
+
+echo  "Initialize User: ${HOSTNAME}  ${newUser}"
+
+# ユーザーを追加する。
+newUserAddOpts="-g ${newUserGroup}  -d /home/${newUser}  -s /bin/bash"
+sudo  useradd  ${newUserAddOpts}  -m  ${newUser}
+eval  newUserHome=~${newUser}
+
+# ユーザーのパスワードを設定する。
+newPasswd=${userPass}
+sedPat="^${newUser}:!:.*\$"
+sedRep="${newUser}:${newPasswd}:18294:0:99999:7:::"
+sedCmd="s|${sedPat}|${sedRep}|"
+command="sudo  sed -i.bak  -e '${sedCmd}'  /etc/shadow"
+echo  "Execute: ${command}"
+eval  ${command}
