a3afca2d7d79147d0955d99a98347f00b4bcc57b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-03-01 19:05:13 +0900

    仮想マシンに RAM Disk を設定

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -6,4 +6,11 @@ echo  Provisioning $HOSTNAME
 
 sudo  timedatectl  set-timezone Asia/Tokyo
 
+# RamDisk
+sudo  mkdir        /ramdisk
+sudo  chmod  1777  /ramdisk
+echo  -e  "tmpfs\t/ramdisk\ttmpfs\trw,size=2048m,x-gvfs-show\t0\t0"  \
+      |  sudo  tee -a  /etc/fstab
+sudo  mount  -a
+
 date  >  /root/.provision.root
