fae15ea01ecf9045dfda48aa2580f6d003aa674e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-06-28 11:57:14 +0900

    pyenv を設定

--- vagrant/StartPyEnv.sh
+++ vagrant/StartPyEnv.sh
@@ -2,7 +2,7 @@
 
 pyenvRoot=${HOME}/.pyenv
 
+export  PYENV_VIRTUALENV_DISABLE_PROMPT=1
 export  PYENV_ROOT=${pyenvRoot}
 export  PATH=${pyenvRoot}/bin:${PATH}
 eval    "$(pyenv  init  -)"
-
--- vagrant/provision.pyenv.sh
+++ vagrant/provision.pyenv.sh
@@ -1,4 +1,4 @@
-#!/bin/bash  -xue
+#!/bin/bash  -xe
 
 echo  Provisioning  PyEnv  ${USER} for ${HOSTNAME}
 test  -f  ${HOME}/.provision.pyenv  &&  exit 0
@@ -8,5 +8,14 @@ venvPath=${pyenvRoot}/plugins/pyenv-virtualenv
 
 git  clone  https://github.com/yyuu/pyenv.git  ${pyenvRoot}
 git  clone  https://github.com/yyuu/pyenv-virtualenv.git  ${venvPath}
+
+source  ${HOME}/LocalSettings/Bash/StartPyEnv.sh
+
+venvName='miniconda3-4.3.21'
+pyenv   install   ${venvName}
+pyenv   activate  ${venvName}
+
+pythonRoot=${pyenvRoot}/versions/${venvName}
+pythonBinary=${pythonRoot}/bin/python
 
 date  >  ${HOME}/.provision.pyenv
